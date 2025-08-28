8431aeb81096382f6e54ae72fb76e7d3d97066c1
Shlomi Philip <shlomi.philip@mail.huji.ac.il>
2025-08-28 20:03:30 +0300

    Fix compilation errors in AIModel and AIWrapper

--- src/main/java/com/Shlomi/englishapp/English_conversation_tutor/domain/AIModel.java
+++ src/main/java/com/Shlomi/englishapp/English_conversation_tutor/domain/AIModel.java
@@ -1,7 +1,12 @@
+package com.Shlomi.englishapp.English_conversation_tutor.domain;
+
+import org.springframework.stereotype.Component;
 import com.Shlomi.englishapp.English_conversation_tutor.conversation.AIMessage;
 import com.Shlomi.englishapp.English_conversation_tutor.conversation.UserMessage;
+import com.Shlomi.englishapp.English_conversation_tutor.domain.ChatModel;
 
-class AIModel implements ChatModel{
+@Component
+public class AIModel implements ChatModel{
 
     @Override
     public AIMessage getResponse(UserMessage userMessage){
--- src/main/java/com/Shlomi/englishapp/English_conversation_tutor/domain/AIWrapper.java
+++ src/main/java/com/Shlomi/englishapp/English_conversation_tutor/domain/AIWrapper.java
@@ -1,8 +1,10 @@
 package com.Shlomi.englishapp.English_conversation_tutor.domain;
 
+import org.springframework.stereotype.Component;
 import com.Shlomi.englishapp.English_conversation_tutor.conversation.AIMessage;
 import com.Shlomi.englishapp.English_conversation_tutor.conversation.UserMessage;
 
+@Component
 public class AIWrapper{
     private final AIModel aiModel = new AIModel();
 
