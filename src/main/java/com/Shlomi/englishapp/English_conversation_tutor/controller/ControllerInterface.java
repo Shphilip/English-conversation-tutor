6c2f15ba76b1c028c79b4bd6cdfee0f72d25c312
Philip Shlomi <162868335+Shphilip@users.noreply.github.com>
2025-11-30 09:27:49 +0200

    Refactor ControllerInterface to use UserMessageRequest

--- src/main/java/com/Shlomi/englishapp/English_conversation_tutor/controller/ControllerInterface.java
+++ src/main/java/com/Shlomi/englishapp/English_conversation_tutor/controller/ControllerInterface.java
@@ -1,18 +1,6 @@
 package com.Shlomi.englishapp.English_conversation_tutor.controller;
-
 import com.Shlomi.englishapp.English_conversation_tutor.entities.MessageInterface;
 import com.Shlomi.englishapp.English_conversation_tutor.entities.UserMessageRequest;
-
-// import org.springframework.boot.autoconfigure.security.SecurityProperties.User;
-
-// import com.Shlomi.englishapp.English_conversation_tutor.entities.MessageInterface;
-// import com.Shlomi.englishapp.English_conversation_tutor.entities.MessageInterface;
-// import com.Shlomi.englishapp.English_conversation_tutor.entities.UserMessage;
-// import com.Shlomi.englishapp.English_conversation_tutor.entities.handleRequestInterface;
-
-// public interface ControllerInterface {
-//     MessageInterface handleRequest(MessageInterface  message);
-// }
 
 public interface ControllerInterface {
     MessageInterface handleRequest(UserMessageRequest message);
